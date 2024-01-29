9f1583922aa349e859069ba7124af106f7131abb
David Patrício <david.c.patricio@gmail.com>
2024-01-29 14:40:33 +0000

    Configure pages and routes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const buttonClicked = useSelector((state) => state.example.buttonClicked);
-
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to="/login">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/teste">
+      <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
-      {buttonClicked ? 'Clicked' : 'Not clicked'}
     </Nav>
   );
 }
