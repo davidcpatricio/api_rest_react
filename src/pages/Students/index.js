ea955943ecac453bf38a5798105a4623b48ab794
David Patrício <david.c.patricio@gmail.com>
2024-02-07 12:44:49 +0000

    Deleting students from API

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { get } from 'lodash';
 import { Link } from 'react-router-dom';
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+import {
+  FaUserCircle,
+  FaEdit,
+  FaWindowClose,
+  FaExclamation,
+} from 'react-icons/fa';
+import { toast } from 'react-toastify';
 
 import { Container } from '../../styles/GlobalStyles';
 import { ProfilePicture, StudentContainer } from './styled';
 import axios from '../../services/axios';
-
 import Loading from '../../components/loading';
 
 export default function Students() {
@@ -23,6 +28,37 @@ export default function Students() {
 
     getData();
   }, []);
+
+  const handleDeleteConfirm = (e) => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute('display', 'block');
+    e.currentTarget.remove();
+  };
+
+  const handleDelete = async (e, id, index) => {
+    e.persist();
+
+    try {
+      setIsLoading(true);
+      await axios.delete(`/students/${id}`);
+      const newStudents = [...students];
+      newStudents.splice(index, 1);
+      setStudents(newStudents);
+      setIsLoading(false);
+    } catch (err) {
+      const status = get(err, 'response.data.status', []);
+
+      if (status === 401) {
+        toast.error('You must be logged in.');
+      } else {
+        toast.error('An unknown error ocurred while attempting to delete.');
+      }
+
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
@@ -30,7 +66,7 @@ export default function Students() {
       <h1>Students</h1>
 
       <StudentContainer>
-        {students.map((student) => (
+        {students.map((student, index) => (
           <div key={String(student.id)}>
             <ProfilePicture>
               {get(student, 'Photos[0].url', false) ? (
@@ -47,9 +83,19 @@ export default function Students() {
               <FaEdit size={16} />
             </Link>
 
-            <Link to={`/student/${student.id}/delete`}>
+            <Link
+              onClick={handleDeleteConfirm}
+              to={`/student/${student.id}/delete`}
+            >
               <FaWindowClose size={16} />
             </Link>
+
+            <FaExclamation
+              size={16}
+              display="none"
+              cursor="pointer"
+              onClick={(e) => handleDelete(e, student.id, index)}
+            />
           </div>
         ))}
       </StudentContainer>
